42efb809d9f136aa4caa11042b1c758cc4330448
chenchao36 <1335334316@qq.com>
2020-08-17 19:08:13 +0800

    git 提交测试

---
+++ maven_practise/src/main/java/com/hik/App.java
@@ -0,0 +1,16 @@
+package com.hik;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("驱动初始化成功");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
